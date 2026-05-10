634f09bdeaf3a69d07da8045e5f60f2671a6559d
Jacob Arrazola <jakob.234244@gmail.com>
2026-05-10 00:42:17 -0600

    Setup Supabase connection with env variables

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
+console.log("Gemini Key Loaded:", !!import.meta.env.VITE_GEMINI_API_KEY);
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
